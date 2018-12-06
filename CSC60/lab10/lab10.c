dce5072e1ebecaa5dded59a3ef5204981301bba7
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2018-12-06 15:39:25 -0800

    Removed comment sections of process_input function

--- CSC60/lab10/lab10.c
+++ CSC60/lab10/lab10.c
@@ -149,19 +149,19 @@ int parseline(char *cmdline, char **argv)
 /* ----------------------------------------------------------------- */
 /*                  process_input                                    */
 /* ----------------------------------------------------------------- */
-/*void process_input(int argc, char **argv) {                       */
+void process_input(int argc, char **argv) {                       
 
 /* Step 1: Call handle_redir to deal with operators:            */
 /* < , or  >, or both                                           */
 
 /* Step 2: perform system call execvp to execute command        */
 /* Hint: Please be sure to review execvp.c sample program       */
-/* if (........ == -1) {                                        */
-/*    fprintf(stderr, "Error on the exec call\n");              */
-/*    _exit(EXIT_FAILURE);                                      */
-/* }                                                            */
+ if (........ == -1) {                                        
+    fprintf(stderr, "Error on the exec call\n");              
+    _exit(EXIT_FAILURE);                                      
+ }                                                            
 
-// }
+ }
 /* ----------------------------------------------------------------- */
 //void handle_redir(int count, char *argv[])
 /* ----------------------------------------------------------------- */
