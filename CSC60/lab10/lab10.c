9a3c4d87cee02a011c080e2b29675f4a5bd1ee59
matt2ology <34324060+matt2ology@users.noreply.github.com>
2018-12-09 00:25:00 -0800

    Removed single line comments in else fork off process.

--- CSC60/lab10/lab10.c
+++ CSC60/lab10/lab10.c
@@ -102,27 +102,27 @@ int main(void)
             printf("%s: Command not found.\n", cmdline);
 
             //.......................IGNORE........................
-            //	/* Else, fork off a process */
-            //      else {
-            //	    pid = fork();
-            //          switch(pid)
-            //    	    {
-            //	    	case -1:
-            //		    perror("Shell Program fork error");
-            //	            exit(EXIT_FAILURE);
-            //	   	case 0:
-            //		    /* I am child process. I will execute the command, */
-            //		    /* and call: execvp */
-            //		    process_input(argc, argv);
-            //		    break;
-            //	   	default:
-            //		    /* I am parent process */
-            //		    if (wait(&status) == -1)
-            //		    	perror("Parent Process error");
-            //		    else
-            //		   	printf("Child returned status: %d\n",status);
-            //		    break;
-            //	    } 	/* end of the switch */
+        	/* Else, fork off a process */
+              else {
+        	    pid = fork();
+                  switch(pid)
+            	    {
+        	    	case -1:
+        		    perror("Shell Program fork error");
+        	            exit(EXIT_FAILURE);
+        	   	case 0:
+        		    /* I am child process. I will execute the command, */
+        		    /* and call: execvp */
+        		    process_input(argc, argv);
+        		    break;
+        	   	default:
+        		    /* I am parent process */
+        		    if (wait(&status) == -1)
+        		    	perror("Parent Process error");
+        		    else
+        		   	printf("Child returned status: %d\n",status);
+        		    break;
+        	    } 	/* end of the switch */
             //...end of the IGNORE above.........................
         } /* end of the if-else-if */
     }     /* end of the while */
