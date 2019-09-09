7360bbedf079fbcb91f1ed86190d10486974a3e9
matt2ology <34324060+matt2ology@users.noreply.github.com>
2019-09-08 19:18:55 -0700

    Init commit.

---
+++ csc131-computer-software-engineering/assignments/2-subHunterRefactored/subHunterRefactored/app/src/main/java/com/example/subhunterrefactored/Submarine.java
@@ -0,0 +1,20 @@
+public class Submarine extends UnderwaterVehicle {
+    private int subHorizontalPosition = 0;
+    private int subVerticalPosition = 0;
+    private boolean isHit = false;
+    int getSubHorizontalPosition(){
+
+    }
+    int setSubHorizontalPosition(){
+
+    }
+    int getSubVerticalPosition(){
+
+    }
+    int setSubVerticalPosition(){
+
+    }
+    boolean subIsHit(){
+
+    }
+}
