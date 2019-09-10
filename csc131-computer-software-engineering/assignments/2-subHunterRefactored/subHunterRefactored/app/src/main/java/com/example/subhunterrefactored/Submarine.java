45b7b44c9f13f65c635ed7fc0893ae3fdf0ca07e
matt2ology <34324060+matt2ology@users.noreply.github.com>
2019-09-09 22:52:23 -0700

    Renamed and defined function.

--- csc131-computer-software-engineering/assignments/2-subHunterRefactored/subHunterRefactored/app/src/main/java/com/example/subhunterrefactored/Submarine.java
+++ csc131-computer-software-engineering/assignments/2-subHunterRefactored/subHunterRefactored/app/src/main/java/com/example/subhunterrefactored/Submarine.java
@@ -17,8 +17,8 @@ public class Submarine extends UnderwaterVehicle {
     int setSubHorizontalPosition(int gridHorizontalXPosition){
         subHorizontalPosition = gridHorizontalXPosition;
     }
-    int getSubVerticalPosition(){
-
+    int getSub_Yposition(){
+        return sub_Yposition;
     }
     int setSub_Yposition(int gridLongitude){
         sub_Yposition = gridLongitude;
