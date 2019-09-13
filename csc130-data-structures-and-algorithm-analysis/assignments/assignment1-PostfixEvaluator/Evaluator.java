af067bcb863ebfcbd31d7905b0259b07706f26f5
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2019-09-12 22:08:31 -0700

    init commit.

---
+++ csc130-data-structures-and-algorithm-analysis/assignments/assignment1-PostfixEvaluator/Evaluator.java
@@ -0,0 +1,46 @@
+/*=============================================================================
+AUTHOR: Matthew Mendoza
+CSC 130: Data Structures and Algorithm Analysis - Fall 2019
+Assignment #: Assignment_Name
+=============================================================================*/
+/*
+ * The code that evaluates the input queue must use the stack-based approach we
+ * went over in class. Naturally, your solution will contain both a private 
+ * queue and a private stack. How you implement these is completely up to 
+ * you – but I would strongly recommend using a linked-list.
+ */
+public class Evaluator {
+    /**
+     * COMMENTS DESCRIBING WHAT THE METHOD/CONSTRUCTOR DOES
+     */
+    public Evaluator() {
+
+    }
+
+    /**
+     * @return Returns text about you – the author of this class.
+     * @author Matthew Mendoza
+     */
+    public String about() {
+        return String.format("Hello, I'm Matt. \n");
+    }
+
+    /**
+     * @return Enqueue the item onto the postfix input queue.
+     */
+    public void enqueue(String item) {
+
+    }
+
+    /**
+     * The code that evaluates the input queue must use the stack-based approach we
+     * went over in class. Naturally, your solution will contain both a private
+     * queue and a private stack. How you implement these is completely up to you –
+     * but I would strongly recommend using a linked-list.
+     * 
+     * @return Evaluates the values and returns a double result.
+     */
+    public double evaluate() {
+
+    }
+} // END OF Evaluator CLASS
