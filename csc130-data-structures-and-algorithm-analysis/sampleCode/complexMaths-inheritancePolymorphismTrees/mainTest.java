da8544344c7be06651e67671eeadfa1c6a874b5b
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2019-09-25 14:53:48 -0700

    Remove arraylist import; for, it was not used.

--- csc130-data-structures-and-algorithm-analysis/sampleCode/complexMaths-inheritancePolymorphismTrees/mainTest.java
+++ csc130-data-structures-and-algorithm-analysis/sampleCode/complexMaths-inheritancePolymorphismTrees/mainTest.java
@@ -1,5 +1,3 @@
-
-import java.util.ArrayList;
 public class mainTest {
 	public static void main(String[] args) {
 		Node vn = new ValueNode();
@@ -8,7 +6,7 @@ public class mainTest {
 		DivisionNode d = new DivisionNode();
 		AdditionNode a = new AdditionNode();
 		SubtractionNode s = new SubtractionNode();
-		
+
 		ONL.include(m);
 		ONL.include(d);
 		ONL.include(a);
@@ -16,9 +14,9 @@ public class mainTest {
 		ONL.setLeft(vn.evaluate());
 		ONL.setRight(vn.evaluate());
 
-		System.out.printf("%d * %d = %d%n",ONL.getLeft(0),ONL.getRight(0),ONL.evaluate(0));
-		System.out.printf("%d / %d = %d%n",ONL.getLeft(1),ONL.getRight(1),ONL.evaluate(1));
-		System.out.printf("%d + %d = %d%n",ONL.getLeft(2),ONL.getRight(2),ONL.evaluate(2));
-		System.out.printf("%d - %d = %d%n",ONL.getLeft(3),ONL.getRight(3),ONL.evaluate(3));
+		System.out.printf("%d * %d = %d%n", ONL.getLeft(0), ONL.getRight(0), ONL.evaluate(0));
+		System.out.printf("%d / %d = %d%n", ONL.getLeft(1), ONL.getRight(1), ONL.evaluate(1));
+		System.out.printf("%d + %d = %d%n", ONL.getLeft(2), ONL.getRight(2), ONL.evaluate(2));
+		System.out.printf("%d - %d = %d%n", ONL.getLeft(3), ONL.getRight(3), ONL.evaluate(3));
 	}
 }
