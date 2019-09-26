f9dc0e4dff586e93933cbd3167c0e24c0d004359
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2019-09-26 13:55:35 -0700

    Changed values of nodes.

--- csc130-data-structures-and-algorithm-analysis/assignments/assignment2-BasicBinaryTree/mainBasicBinaryTreeTest.java
+++ csc130-data-structures-and-algorithm-analysis/assignments/assignment2-BasicBinaryTree/mainBasicBinaryTreeTest.java
@@ -13,10 +13,7 @@ public class mainBasicBinaryTreeTest {
         tree.root.left = new Node(3);
         tree.root.left.left = new Node(42);
         tree.root.left.right = new Node(26);
-        
-        tree.root.right.left = new Node(7);
-        tree.root.right.right = new Node(74);
-        
+        tree.root.right = new Node(5);
         tree.root.right.left = new Node(7);
         tree.root.right.left.left = new Node(9);
         tree.root.right.left.right = new Node(12);
