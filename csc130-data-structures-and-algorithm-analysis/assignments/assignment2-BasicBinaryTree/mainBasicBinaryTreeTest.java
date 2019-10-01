9bc92cc271d62654d369595e1d6169b2816b2293
matt2ology <34324060+matt2ology@users.noreply.github.com>
2019-10-01 00:43:27 -0700

    Removed previous tree structure.

--- csc130-data-structures-and-algorithm-analysis/assignments/assignment2-BasicBinaryTree/mainBasicBinaryTreeTest.java
+++ csc130-data-structures-and-algorithm-analysis/assignments/assignment2-BasicBinaryTree/mainBasicBinaryTreeTest.java
@@ -26,15 +26,7 @@ Assignment #2 – Basic Binary Tree
  */
 public class mainBasicBinaryTreeTest {
     public static void main(String args[]) {
-        /* create root */
-        // tree.root = new Node(1);
-        // tree.root.left = new Node(3);
-        // tree.root.left.left = new Node(42);
-        // tree.root.left.right = new Node(26);
-        // tree.root.right = new Node(5);
-        // tree.root.right.left = new Node(7);
-        // tree.root.right.left.left = new Node(9);
-        // tree.root.right.left.right = new Node(12);
+
         BinaryTree tree = new BinaryTree();
 
         /* create root */
