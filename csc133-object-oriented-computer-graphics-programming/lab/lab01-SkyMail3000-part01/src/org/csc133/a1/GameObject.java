27d3ecc30c9fbe313435ed07b9c196f13443d148
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-10-11 21:42:37 -0700

    Added x max width as 1024 px

--- csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/GameObject.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/GameObject.java
@@ -13,5 +13,7 @@ package org.csc133.a1;
 public abstract class GameObject {
     // All game objects have a location
     private float location;
+    // non-negative values X initially should be in the range 0.0 to 1024.0
+    public static final int WORLD_WIDTH = 1024;
 
 }
