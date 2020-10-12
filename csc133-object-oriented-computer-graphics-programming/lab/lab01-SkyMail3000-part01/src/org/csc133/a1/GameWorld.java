c8d81176f326f32aae19d9ec711d22d1d7247450
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-10-11 19:00:40 -0700

    Init commit manipulate and display the data and game state values in the game model

---
+++ csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/GameWorld.java
@@ -0,0 +1,11 @@
+package org.csc133.a1;
+
+public class GameWorld {
+    public void init() {
+        // code here to create the
+        // initial game objects/setup
+    }
+    // additional methods here to
+    // manipulate world objects and
+    // related game state data
+}
