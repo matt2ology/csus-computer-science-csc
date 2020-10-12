47f9319c74c6e4367442a305ed8b2f75f0c807f5
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-10-12 01:44:45 -0700

    init commit

---
+++ csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/Fixed.java
@@ -0,0 +1,16 @@
+package org.csc133.a1;
+
+public class Fixed extends GameObject {
+    private static int sequenceNumberID = 0;
+
+    public Fixed() {
+        sequenceNumberID++;
+    }
+
+    /**
+     * @return Returns the next sequenceNumberID for the SkyScrapers to distinguish
+     */
+    public int GetsequenceNumberID() {
+        return sequenceNumberID;
+    }
+}
