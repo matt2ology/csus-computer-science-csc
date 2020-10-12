44d9998f2b2551939cb93418d83ccad6e5fd75b2
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-10-12 03:19:23 -0700

    Not all fixed obj have sequenceNumberID

--- csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/Fixed.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/Fixed.java
@@ -2,16 +2,16 @@ package org.csc133.a1;
 
 // All fixed game objects are not allowed to change location once they are created.
 public class Fixed extends GameObject {
-    private static int sequenceNumberID = 0;
 
-    public Fixed() {
-        sequenceNumberID++;
+    public Fixed(int colorValue) {
+        super(colorValue);
     }
 
-    /**
-     * @return Returns the next sequenceNumberID for the SkyScrapers to distinguish
-     */
-    public int getSequenceNumberID() {
-        return sequenceNumberID;
+    public Fixed(int colorValue, int sizeValue) {
+        super(colorValue, sizeValue);
+    }
+
+    public Fixed(int redValue, int greenValue, int blueValue, int sizeValue) {
+        super(redValue, greenValue, blueValue, sizeValue);
     }
 }
