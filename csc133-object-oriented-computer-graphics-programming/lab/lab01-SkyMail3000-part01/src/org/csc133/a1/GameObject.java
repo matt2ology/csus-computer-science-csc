6eb970279415b14d3c5cfb190491d251588f7e0d
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-10-11 22:16:32 -0700

    Added color attribute

--- csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/GameObject.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/GameObject.java
@@ -35,6 +35,8 @@ public abstract class GameObject {
 
     // All game objects have a location
     private Vector<Float> position;
+    // All game objects have a color
+    private int color;
     // non-negative values X initially should be in the range 0.0 to 1024.0
     public static final int WORLD_WIDTH = 1024;
     // non-negative values Y initially should be in the range 0.0 to 768.0
