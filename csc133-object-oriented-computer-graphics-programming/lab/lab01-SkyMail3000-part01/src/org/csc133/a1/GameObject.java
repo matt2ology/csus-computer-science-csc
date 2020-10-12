ec56428264f82733cd64b274db8c7b49824d1e6b
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-10-11 21:04:53 -0700

    Holds collection of game objects+ state variables

---
+++ csc133-object-oriented-computer-graphics-programming/lab/lab01-SkyMail3000-part01/src/org/csc133/a1/GameObject.java
@@ -0,0 +1,17 @@
+package org.csc133.a1;
+
+/**
+ * <b>Holds a collection of game objects and other state variables.</b><br>
+ * <br>
+ * The game world contains a collection which aggregates objects of abstract
+ * type GameObject.
+ * 
+ * There are <b>two kinds of abstract game objects</b> called: <b>fixed
+ * objects</b> of type <code>Fixed</code> with fixed locations <i>(which are
+ * fixed in place)</i> and <b>moveable objects</b> of type <code>Movable</code>
+ * with changeable locations <i>(which can move or be moved about the
+ * world).</i>
+ */
+public class GameObject {
+    
+}
