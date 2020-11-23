f457cdebf9692827b2445f865739aff458c386de
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-23 03:04:34 -0800

    Commented requirements

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/GameObject.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/GameObject.java
@@ -2,10 +2,15 @@ package org.csc133.a2;
 
 /**
  * The game world contains a collection which aggregates objects of abstract
- * type <code>GameObject</code>. There are two kinds of abstract game objects called: fixed
- * objects of type <code>Fixed</code> with fixed locations (which are fixed in place) and
- * moveable objects of type <code>Movable</code> with changeable locations (which can move or
- * be moved about the world).
+ * type <code>GameObject</code>. There are two kinds of abstract game objects
+ * called: fixed objects of type <code>Fixed</code> with fixed locations (which
+ * are fixed in place) and moveable objects of type <code>Movable</code> with
+ * changeable locations (which can move or be moved about the world).
  */
 public abstract class GameObject {
+    /**
+     * All game objects have a location, defined by floating point (you can use
+     * float or double to represent it) non-negative values X and Y which initially
+     * should be in the range 0.0 to 1024.0 and 0.0 to 768.0, respectively.
+     */
 }
