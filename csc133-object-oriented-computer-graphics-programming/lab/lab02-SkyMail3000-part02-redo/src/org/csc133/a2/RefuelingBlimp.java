147d506ce946ab4c37f4a196e5f154e0e37540aa
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 22:59:18 -0800

    Extends Fixed

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/RefuelingBlimp.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/RefuelingBlimp.java
@@ -2,7 +2,7 @@
  * RefuelingBlimp is a fixed object. <br>
  * <br>
  */
-public class RefuelingBlimp {
+public class RefuelingBlimp extends Fixed{
 
     // Sizes of the rest of the game objects are chosen randomly when created, and
     // constrained to a reasonable positive integer value (e.g., between 10 to 50).
