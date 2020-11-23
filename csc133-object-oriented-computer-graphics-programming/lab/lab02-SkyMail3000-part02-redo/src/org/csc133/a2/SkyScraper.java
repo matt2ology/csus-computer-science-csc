00caf70d345afbb4ef987e98383d488c1b6c33a6
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 22:29:59 -0800

    format code

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
@@ -1,13 +1,11 @@
 /**
- * SkyScraper is a fixed object. All SkyScrapers have the same size (chosen by
- * you), assigned when they are created (e.g, size of all SkyScrapers can be
- * 10). SkyScrapers are not allowed to change color once they are created.
+ * SkyScraper is a fixed object.
+ */
+public class SkyScraper extends Fixed {
     // All SkyScrapers have the same size (chosen by you), assigned when they are
     // created (e.g, size of all SkyScrapers can be 10). SkyScrapers are not allowed
     // to change color once they are created.
 
- */
-public class SkyScraper extends Fixed {
     // Each SkyScraper is a numbered marker that acts as a waypoint on the flight
     // path
     private int sequenceNumber;
