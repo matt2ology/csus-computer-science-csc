2cbd64bc957ceda546238b3ffcc8ded8dfeb5459
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 23:00:59 -0800

    attribute capacity added

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/RefuelingBlimp.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/RefuelingBlimp.java
@@ -8,4 +8,10 @@ public class RefuelingBlimp extends Fixed{
     // constrained to a reasonable positive integer value (e.g., between 10 to 50).
     // For instance, size of one of the refueling blimp may be 15 while size of
     // another refueling blimp can may be 20.
+    /**
+     * RefuelingBlimps are fixed game objects that have an attribute capacity
+     * (amount of fuel an refueling blimp contains). The initial capacity of the
+     * refueling blimp is proportional to its size.
+     */
+    private int capacity;
 }
