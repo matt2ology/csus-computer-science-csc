bbd10aea2921dbb37a522253b11c20b307b34384
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 22:29:24 -0800

    Moved SkyScrapper requirements for constructer

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
@@ -2,6 +2,10 @@
  * SkyScraper is a fixed object. All SkyScrapers have the same size (chosen by
  * you), assigned when they are created (e.g, size of all SkyScrapers can be
  * 10). SkyScrapers are not allowed to change color once they are created.
+    // All SkyScrapers have the same size (chosen by you), assigned when they are
+    // created (e.g, size of all SkyScrapers can be 10). SkyScrapers are not allowed
+    // to change color once they are created.
+
  */
 public class SkyScraper extends Fixed {
     // Each SkyScraper is a numbered marker that acts as a waypoint on the flight
