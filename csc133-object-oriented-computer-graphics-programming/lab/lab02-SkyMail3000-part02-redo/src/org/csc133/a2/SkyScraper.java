3b0bd9d7baf28313664ae72f739326289c80ee2a
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 22:46:59 -0800

    Changed comment type

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
@@ -6,7 +6,6 @@ public class SkyScraper extends Fixed {
     // created (e.g, size of all SkyScrapers can be 10). SkyScrapers are not allowed
     // to change color once they are created.
 
-    // Each SkyScraper is a numbered marker that acts as a waypoint on the flight
-    // path
+    /**Each SkyScraper is a numbered marker that acts as a waypoint on the flight path */
     private int sequenceNumber;
 }
