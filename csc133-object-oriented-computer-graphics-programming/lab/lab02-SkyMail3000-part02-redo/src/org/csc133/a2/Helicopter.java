019933ae327f371cba7b840e86674de54d56878b
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 22:09:31 -0800

    Added additonal doc comments

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/Helicopter.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/Helicopter.java
@@ -1,7 +1,8 @@
 /**
- * Helicopter is a movable object
+ * Helicopter is a movable object. All helicopters have the same size (chosen by
+ * you), assigned when they are created (e.g, size of all helicopters can be
+ * 40).
  */
 public class Helicopter {
 
-    
 }
--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/RefuelingBlimp.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/RefuelingBlimp.java
@@ -1,7 +1,10 @@
 /**
- * RefuelingBlimp is a fixed object
+ * RefuelingBlimp is a fixed object. Sizes of the rest of the game objects are
+ * chosen randomly when created, and constrained to a reasonable positive
+ * integer value (e.g., between 10 to 50). For instance, size of one of the
+ * refueling blimp may be 15 while size of another refueling blimp can may be 20.
+ * <br><br>
  */
 public class RefuelingBlimp {
 
-    
 }
