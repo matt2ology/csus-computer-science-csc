7a5823103887ac1fedae65d13eb61f76afa94e8c
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-11-22 22:21:17 -0800

    Added more document comments

--- csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
+++ csc133-object-oriented-computer-graphics-programming/lab/lab02-SkyMail3000-part02-redo/src/org/csc133/a2/SkyScraper.java
@@ -1,5 +1,7 @@
 /**
- * SkyScraper is a fixed object
+ * SkyScraper is a fixed object. All SkyScrapers have the same size (chosen by
+ * you), assigned when they are created (e.g, size of all SkyScrapers can be
+ * 10).
  */
 public class SkyScraper {
 
