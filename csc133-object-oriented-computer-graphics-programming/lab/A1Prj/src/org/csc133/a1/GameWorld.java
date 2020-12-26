e031f1646adf23769455a193398fec525549058a
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-12-25 19:13:49 -0800

    Added class description

--- csc133-object-oriented-computer-graphics-programming/lab/A1Prj/src/org/csc133/a1/GameWorld.java
+++ csc133-object-oriented-computer-graphics-programming/lab/A1Prj/src/org/csc133/a1/GameWorld.java
@@ -1,5 +1,9 @@
 package org.csc133.a1;
 
+/**
+ * The game world contains a collection which aggregates objects of abstract
+ * type GameObject.
+ */
 public class GameWorld {
     public void init() {
         // Code here to create the initial game objects/setup
