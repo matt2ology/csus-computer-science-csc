c76865bc44eac69c420a51c8cdf8381aac168a26
matt2ology <34324060+matt2ology@users.noreply.github.com>
2020-12-25 18:00:40 -0800

    Holds a collection of game obj and state var

---
+++ csc133-object-oriented-computer-graphics-programming/lab/A1Prj/src/org/csc133/a1/GameWorld.java
@@ -0,0 +1,8 @@
+package org.csc133.a1;
+
+public class GameWorld {
+    public void init() {
+        // Code here to create the initial game objects/setup
+    }
+    // additonal methods here to manipulate world objects and relate game state data
+}
