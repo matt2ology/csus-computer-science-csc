0d7ebb37d712b75e6e6fc335b1630c0aa31741e3
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-02-20 00:44:31 -0800

    Reason to if abspath(PROGRAM_FILE) == @__FILE__

--- stat196k-analyzing-and-processing-big-data/assignments/assignment00-s-h-template/assigment00-s-h.jl
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment00-s-h-template/assigment00-s-h.jl
@@ -2,4 +2,14 @@ function main()
     # do stuff
 end
 
-main()
+#= 
+When a file is run as the main script using julia file.jl one might want to 
+activate extra functionality like command line argument handling. A way to 
+determine that a file is run in this fashion is to check 
+if abspath(PROGRAM_FILE) == @__FILE__ is true.
+---
+https://docs.julialang.org/en/v1/manual/faq/#How-do-I-check-if-the-current-file-is-being-run-as-the-main-script?
+=#
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
