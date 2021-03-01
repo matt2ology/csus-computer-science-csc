4ebabfd625f2d85bbc91af6c5d1b7128eeaed6c7
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-03-01 03:15:12 -0800

    Formatted and updated stat distributions used

--- stat196k-analyzing-and-processing-big-data/assignments/assignment07-s-qq-plot-graphical-methods-for-comparing-distributions/assigment07-s-graphical-methods-for-comparing-distributions.jl
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment07-s-qq-plot-graphical-methods-for-comparing-distributions/assigment07-s-graphical-methods-for-comparing-distributions.jl
@@ -4,42 +4,45 @@ using Distributions
 using StatsPlots
 
 function main()
-    # OUTPUT_FOLDER_PATH = "output-plot-results-assignment/"
-    # mkdir("output-plot-results-assignment")
-    # Levy Distribution
-    ## The Levy Distribution is a probability distribution that is both continuous—for non-negative random variables—and stable.
-    levyDistribution = Levy(5)
-    # Plots the probability density function (PDF) of the above Levy Distribution
-    savefig(plot(levyDistribution, title="Levy Distribution With 5 Degrees of Freedom"), "levy-distribution-with-5-degrees-of-freedom")
+    OUTPUT_FOLDER_PATH = "output-plot-results-assignment/"
+    mkdir("output-plot-results-assignment")
+
+    # Cosine Distribution
+    ## The cosine Distribution is a probability distribution that is both continuous—for non-negative random variables—and stable.
+    cosine_distribution = Cosine(5)
+    # Plots the probability density function (PDF) of the above cosine Distribution
+    savefig(plot(cosine_distribution, lw=5 ,title="cosine Distribution With 5 Degrees of Freedom"), 
+            OUTPUT_FOLDER_PATH * "cosine-distribution-with-5-degrees-of-freedom")
 
     # Histograms
     ## The shape of the histogram of n IID samples from a distribution will match the PDF as n goes to infinity.
     ## This suggests a quick way to test if data came from a distribution is to look at the histogram.
-    savefig(histogram(rand(levyDistribution, 100), title="100 observations"), "histogram-of-n-iid-samples-hundred")
-    savefig(histogram(rand(levyDistribution, 10000), title="10 thousand observations"), "histogram-of-n-iid-samples-thousands")
+    savefig(histogram(rand(cosine_distribution, 100), title="100 observations"), 
+            OUTPUT_FOLDER_PATH * "histogram-of-n-iid-samples-hundred")
+    savefig(histogram(rand(cosine_distribution, 10000), title="10 thousand observations"), 
+            OUTPUT_FOLDER_PATH * "histogram-of-n-iid-samples-thousands")
 
     # Quantiles
     ## The p quantile for a distribution X is the point xq that satisfies P(X <= xq) = p
     q = range(0, 1, step=0.1)
-
     collect(q)
-    
-    quantile(levyDistribution, q)
+    quantile(cosine_distribution, q)
 
     # Simulate n data points from this RV coefficient.
     n = 100
-    x_from_levyDistribution = rand(levyDistribution, n)
+    x_from_cosine_distribution = rand(cosine_distribution, n)
 
-    savefig(qqplot(x_from_levyDistribution, levyDistribution, title="Data Appears To Match Distribution"), "data-appears-to-match-distribution")
-    maximum(x_from_levyDistribution)
+    savefig(qqplot(x_from_cosine_distribution, cosine_distribution, title="Data Appears To Match Distribution"), 
+            OUTPUT_FOLDER_PATH * "data-appears-to-match-distribution")
 
-    # Represents a Uniform random variable
-    normalDistribution15 = Normal(0, 15)
+    maximum(x_from_cosine_distribution)
 
-    savefig(qqplot(x_from_levyDistribution, normalDistribution15, title="Data DOES NOT match distribution"), "data-does-not-match-distribution")
+    # Represents a Arcsine random variable
+    arcsine_distribution15 = Arcsine(0, 15)
 
+    savefig(qqplot(x_from_cosine_distribution, arcsine_distribution15, title="Data DOES NOT match distribution"), 
+            OUTPUT_FOLDER_PATH * "data-does-not-match-distribution")
 end
-
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main()
