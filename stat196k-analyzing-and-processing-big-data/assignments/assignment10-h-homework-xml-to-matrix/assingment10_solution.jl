f709e011d3400e6c20058ca037dd6a51d62fda2a
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-03-15 23:35:10 -0700

    init commit

---
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-homework-xml-to-matrix/assingment10_solution.jl
@@ -0,0 +1,16 @@
+#= 
+Assignment 10 - Homework XML To Matrix
+Use NLP to convert the text descriptions of each nonprofit into the rows of one
+big document term matrix. The resulting matrix will have one row for every 990
+form that has a description, and one column for every word in the lexicon. In
+the next assignment we will use this matrix to cluster the nonprofits based on
+their descriptions. 
+=#
+
+function main()
+    # do stuff
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
