1fa5f6b77b7d5b25dd3509995ec3b619768776d8
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-03-26 01:09:31 -0700

    understanding reached ready to make functions

--- stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-xml-to-matrix/assingment10_solution.jl
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-xml-to-matrix/assingment10_solution.jl
@@ -4,15 +4,24 @@ Use NLP to convert the text descriptions of each nonprofit into the rows of one
 big document term matrix. The resulting matrix will have one row for every 990
 form that has a description, and one column for every word in the lexicon. In
 the next assignment we will use this matrix to cluster the nonprofits based on
-their descriptions. 
-=#
+their descriptions. =#
 
 using EzXML # For XML/HTML handling tools for primates
 
+
+
 function main()
-    # do stuff
+    IRS990_FILEPATHS = readdir("data/2019", join=true) # Array of file paths
+    document = readxml("201900079349100000_public.xml")
+
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main()
 end
+
+# //BusinessNameLine1Txt1/text()
+# //Desc/text()
+## //IRS990/Description/text()
+## //ActivityOrMissionDesc/text()
+# //CYTotalRevenueAmnt
