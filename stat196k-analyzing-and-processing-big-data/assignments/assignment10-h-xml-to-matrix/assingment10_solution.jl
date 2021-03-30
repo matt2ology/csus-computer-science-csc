58879f4fb50585c3d5ca77b5ead9f65db657c049
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-03-29 20:37:55 -0700

    Functions refactored

--- stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-xml-to-matrix/assingment10_solution.jl
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-xml-to-matrix/assingment10_solution.jl
@@ -22,4 +22,95 @@ end
 ##     c1 = getcontent(document, "BusinessNameLine1Txt/text()")
 ## end
 
-
+"""
+    get_organization_name(document,array_of_xpath_strings)
+Takes in a document and an array of _Organization Name_ Xpath strings to find,
+and returns the first one that matches the document. If Xpath not found will
+return `missing`
+"""
+function get_organization_name(document, array_of_xpath_strings)
+    @info "Searching for organization_name"
+    for xpath in array_of_xpath_strings
+        found_xml_node = findfirst(xpath, document)
+        """
+        Evaluates `found_xml_node` if the value is not `nothing` the respective
+            Xpath value will be assigned
+        """
+        if !isnothing(found_xml_node)
+            @info "FOUND: organization_name"
+            return nodecontent(found_xml_node)
+        end
+    end
+    @info "NOT FOUND: organization_name"
+    # If none of the Xpath values are found `missing` will be returned
+    return missing
+end
+"""
+    get_nonprofit_description(document,array_of_xpath_strings)
+Takes in a document and an array of _Nonprofit Description_ Xpath strings to find,
+and returns the first one that matches the document. If Xpath not found will
+return `missing`
+"""
+function get_nonprofit_description(document, array_of_xpath_strings)
+    @info "Searching for nonprofit_description"
+    for xpath in array_of_xpath_strings
+        found_xml_node = findfirst(xpath, document)
+        """
+        Evaluates `found_xml_node` if the value is not `nothing` the respective
+            Xpath value will be assigned
+        """
+        if !isnothing(found_xml_node)
+            @info "FOUND: nonprofit_description"
+            return nodecontent(found_xml_node)
+        end
+    end
+    @info "NOT FOUND: nonprofit_description"
+    # If none of the Xpath values are found `missing` will be returned
+    return missing
+end
+"""
+    get_revenue(document,array_of_xpath_strings)
+Takes in a document and an array of _Revenue_ Xpath strings to find,
+and returns the first one that matches the document. If Xpath not found will
+return `missing`
+"""
+function get_revenue(document, array_of_xpath_strings)
+    @info "Searching for revenue"
+    for xpath in array_of_xpath_strings
+        found_xml_node = findfirst(xpath, document)
+        """
+        Evaluates `found_xml_node` if the value is not `nothing` the respective
+            Xpath value will be assigned
+        """
+        if !isnothing(found_xml_node)
+            @info "FOUND: revenue"
+            return nodecontent(found_xml_node)
+        end
+    end
+    @info "NOT FOUND: revenue"
+    # If none of the Xpath values are found `missing` will be returned
+    return missing
+end
+"""
+    get_number_of_employees(document,array_of_xpath_strings)
+Takes in a document and an array of _Number of Employees_ Xpath strings to find,
+and returns the first one that matches the document. If Xpath not found will
+return `missing`
+"""
+function get_number_of_employees(document, array_of_xpath_strings)
+    @info "Searching for number_of_employees"
+    for xpath in array_of_xpath_strings
+        found_xml_node = findfirst(xpath, document)
+        """
+        Evaluates `found_xml_node` if the value is not `nothing` the respective
+            Xpath value will be assigned
+        """
+        if !isnothing(found_xml_node)
+            @info "FOUND: number_of_employees"
+            return nodecontent(found_xml_node)
+        end
+    end
+    @info "NOT FOUND: number_of_employees"
+    # If none of the Xpath values are found `missing` will be returned
+    return missing
+end
