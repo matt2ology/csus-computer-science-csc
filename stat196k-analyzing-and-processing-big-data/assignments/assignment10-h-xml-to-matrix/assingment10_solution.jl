0bc0e60e89c8950b99b88c56c5f8b4437d2e6c4e
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-03-29 23:52:25 -0700

    working state

--- stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-xml-to-matrix/assingment10_solution.jl
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment10-h-xml-to-matrix/assingment10_solution.jl
@@ -7,20 +7,22 @@ the next assignment we will use this matrix to cluster the nonprofits based on
 their descriptions. =#
 
 using EzXML # For XML/HTML handling tools for primates
+using TextAnalysis
+using Serialization
 
-function main()
-    IRS990_FILE = readdir("data/2019/", join=true) # Array of file paths
-
+"""
+    get_content(xml_file)
+A wrapper function to collect the respective data and place results into a dictionary
+"""
+function get_content(xml_file)
+    @info "Getting content"
+    document = readxml(xml_file)
+    organization_name = get_organization_name(document, ["//ReturnHeader/Filer/BusinessName/BusinessNameLine1Txt", "//IRS990/DoingBusinessAsName/BusinessNameLine1Txt"])
+    organization_description = get_nonprofit_description(document, ["//IRS990/ActivityOrMissionDesc", "//IRS990/Desc", "//IRS990EZ/ProgramSrvcAccomplishmentGrp/DescriptionProgramSrvcAccomTxt"])
+    organization_revenue = get_revenue(document, ["//IRS990/RevenueAmt", "//IRS990/TotalRevenueGrp/TotalRevenueColumnAmt"])
+    @info "Building dictionary"
+    Dict("file" => xml_file, "organization" => organization_name, "description" => organization_description, "size_proxy" => organization_revenue)
 end
-
-if abspath(PROGRAM_FILE) == @__FILE__
-    main()
-end
-## for file in IRS990_FILE[1:5]
-##     println(file)
-##     document = readxml(file)
-##     c1 = getcontent(document, "BusinessNameLine1Txt/text()")
-## end
 
 """
     get_organization_name(document,array_of_xpath_strings)
@@ -29,19 +31,20 @@ and returns the first one that matches the document. If Xpath not found will
 return `missing`
 """
 function get_organization_name(document, array_of_xpath_strings)
-    @info "Searching for organization_name..."
+    @info "Searching for organization_name"
     for xpath in array_of_xpath_strings
+        @info "xpath" xpath
         found_xml_node = findfirst(xpath, document)
         """
         Evaluates `found_xml_node` if the value is not `nothing` the respective
             Xpath value will be assigned
         """
         if !isnothing(found_xml_node)
-            @info "FOUND: organization_name"
+            @info "FOUND: organization_name" nodecontent(found_xml_node)
             return nodecontent(found_xml_node)
         end
     end
-    @info "NOT FOUND: organization_name"
+    @warn "NOT FOUND: organization_name"
     # If none of the Xpath values are found `missing` will be returned
     return missing
 end
@@ -53,19 +56,20 @@ and returns the first one that matches the document. If Xpath not found will
 return `missing`
 """
 function get_nonprofit_description(document, array_of_xpath_strings)
-    @info "Searching for nonprofit_description..."
+    @info "Searching for nonprofit_description"
     for xpath in array_of_xpath_strings
+        @info "xpath" xpath
         found_xml_node = findfirst(xpath, document)
         """
         Evaluates `found_xml_node` if the value is not `nothing` the respective
             Xpath value will be assigned
         """
         if !isnothing(found_xml_node)
-            @info "FOUND: nonprofit_description"
+            @info "FOUND: nonprofit_description" nodecontent(found_xml_node)
             return nodecontent(found_xml_node)
         end
     end
-    @info "NOT FOUND: nonprofit_description"
+    @warn "NOT FOUND: nonprofit_description"
     # If none of the Xpath values are found `missing` will be returned
     return missing
 end
@@ -77,19 +81,20 @@ and returns the first one that matches the document. If Xpath not found will
 return `missing`
 """
 function get_revenue(document, array_of_xpath_strings)
-    @info "Searching for revenue..."
+    @info "Searching for revenue"
     for xpath in array_of_xpath_strings
+        @info "xpath" xpath
         found_xml_node = findfirst(xpath, document)
         """
         Evaluates `found_xml_node` if the value is not `nothing` the respective
             Xpath value will be assigned
         """
         if !isnothing(found_xml_node)
-            @info "FOUND: revenue"
+            @info "FOUND: revenue" nodecontent(found_xml_node)
             return nodecontent(found_xml_node)
         end
     end
-    @info "NOT FOUND: revenue"
+    @warn "NOT FOUND: revenue"
     # If none of the Xpath values are found `missing` will be returned
     return missing
 end
@@ -101,19 +106,52 @@ and returns the first one that matches the document. If Xpath not found will
 return `missing`
 """
 function get_number_of_employees(document, array_of_xpath_strings)
-    @info "Searching for number_of_employees..."
+    @info "Searching for number_of_employees"
     for xpath in array_of_xpath_strings
+        @info "xpath" xpath
         found_xml_node = findfirst(xpath, document)
         """
         Evaluates `found_xml_node` if the value is not `nothing` the respective
             Xpath value will be assigned
         """
         if !isnothing(found_xml_node)
-            @info "FOUND: number_of_employees"
+            @info "FOUND: number_of_employees" nodecontent(found_xml_node)
             return nodecontent(found_xml_node)
         end
     end
-    @info "NOT FOUND: number_of_employees"
+    @warn "NOT FOUND: number_of_employees"
     # If none of the Xpath values are found `missing` will be returned
     return missing
+end
+
+function main()
+    IRS990_2019_FILES = readdir("data/2019/", join=true) # Array of file paths
+    
+    dictionary_of_xml_node_text = Dict()
+    dictionary_of_xml_node_text = map(get_content, IRS990_2019_FILES)
+
+    @info "Processing description text"
+    description = [entry["organization_description"] for entry in dictionary_of_xml_node_text]
+    
+    string_test = [StringDocument(" ")]
+    collection_of_files_processed = collect(Int, size(IRS990_2019_FILES))
+    for file in 1:collection_of_files_processed[1]
+        push!(string_test, StringDocument(description[file]))
+    end
+
+    println("The total number of processed description text: ", collection_of_files_processed[1])
+
+    @info "Creating the term document matrix"
+    CORPUS = Corpus(testStrings)
+    remove_case!(CORPUS)
+    prepare!(CORPUS,strip_punctuation)
+    stem!(CORPUS)
+    standardize!(CORPUS, StringDocument)
+    update_lexicon!(CORPUS)
+    document_term_matrix = DocumentTermMatrix(CORPUS)
+    serialize("corpus.jldata", CORPUS)
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
 end
