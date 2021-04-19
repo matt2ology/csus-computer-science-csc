00271512223cc7dc00ffedf4e01ef2051861e97c
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-04-19 03:03:42 -0700

    set up to answer question 1

--- stat196k-analyzing-and-processing-big-data/assignments/assignment15-h-clustering/clustering.jl
+++ stat196k-analyzing-and-processing-big-data/assignments/assignment15-h-clustering/clustering.jl
@@ -1,7 +1,22 @@
+using Serialization
+using Statistics
 function main()
-    # do stuff
+    irs990extract = Serialization.deserialize("./data/irs990extract.jldata")
+    termfreq = Serialization.deserialize("./data/termfreq.jldata")
+    terms = Serialization.deserialize("./data/terms.jldata")
+    number_of_terms_in_document = []
+    number_of_documents = length(irs990extract)
+    for row_of_documents in 1:number_of_documents
+        row = termfreq[row_of_documents,1:end].nzind
+        println(length(row))
+        push!(number_of_terms_in_document,length(row))
+    end
+    println("***")
+    println("Minimum: ",Statistics.minimum(number_of_terms_in_document))
+    println("Maximum: ",Statistics.maximum(number_of_terms_in_document))
+    println("Average: ",Statistics.mean(number_of_terms_in_document))
+    println("***")
 end
-
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main()
