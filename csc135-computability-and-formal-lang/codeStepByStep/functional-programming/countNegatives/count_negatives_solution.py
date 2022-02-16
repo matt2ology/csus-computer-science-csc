934b29fe21353b7fa2cb21eb0c8ca989a1f7d971
matt2ology-lysithea <34324060+matt2ology@users.noreply.github.com>
2022-02-15 18:47:10 -0800

    Add doc string

--- csc135-computability-and-formal-lang/codeStepByStep/functional-programming/countNegatives/count_negatives_solution.py
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/countNegatives/count_negatives_solution.py
@@ -1,3 +1,9 @@
 class CountNegatives:
     def count_negatives(list_of_integers: list[int]) -> int:
+        """
+        Takes a list of integers as a parameter\n
+        Returns: how many numbers in the list are negative\n
+        Example:
+        [5, -1, -3, 20, 47, -10, -8, -4, 0, -6, -6] returns 7
+        """
         return len(list(filter(lambda x: x < 0, list_of_integers)))
