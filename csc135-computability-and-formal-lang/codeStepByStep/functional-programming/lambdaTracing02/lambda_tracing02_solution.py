895f84a951ec9e0582cffedf697ecb4fe018f947
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-02-19 20:30:14 -0800

    lambda_tracing02 test and solution works

--- csc135-computability-and-formal-lang/codeStepByStep/functional-programming/lambdaTracing02/lambda_tracing02_solution.py
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/lambdaTracing02/lambda_tracing02_solution.py
@@ -1,2 +1,2 @@
-class LambdaTracing02:
-    pass
+def lambda_tracing02(numbers: list[int]) -> list[int]:
+    return [n for n in numbers if n % 2 == 0]
---
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/lambdaTracing02/test_lambda_tracing02.py
@@ -0,0 +1,4 @@
+import lambda_tracing02_solution as soln
+
+def test_lambda_tracing02_solution():
+    assert soln.lambda_tracing02([10,-28,33,28,-49,56,49]) == [10, -28, 28, 56]
