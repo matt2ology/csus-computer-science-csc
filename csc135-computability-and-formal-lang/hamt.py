abc9c9c350d2e5fb917c3d392606a54ec5650080
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-02-19 23:27:42 -0800

    Professor's Hash Array Mapped Trie added

---
+++ csc135-computability-and-formal-lang/hamt.py
@@ -0,0 +1,65 @@
+# Hash Array Mapped Trie - Used in CSC 135, Sacramento State
+# Written by Ted Krovetz, February 2022
+# 
+# This implementation assumes that the objects pointed at by the key and value
+# references stored in the HAMT structure do not change during the lifetime
+# of the structure.
+class hamt:
+    
+    DEG = 4      # Children per node (must be power of 2)
+    BITS = 2     # log2(DEG), bits needed to select child
+    MASK = 0b11  # Bitmask for extracting low BITS bits (DEG - 1)
+    
+    def __init__(self, key, value, children=None):
+        self._key = key
+        self._value = value
+        if children == None:
+            self._children = [None] * hamt.DEG
+        else:
+            self._children = children
+    
+    def _set(self, key, value, hashbits):
+        # Each node encountered during search will get altered.
+        # To maintain persistence, each is duplicated, updated, returned.
+        if (self._key == key):
+            # This node matches key. Return duplicate with new value
+            return hamt(self._key, value, self._children)
+        else:
+            # Continue search using hashbits to pick direction
+            child_num = hashbits & hamt.MASK
+            # Copy can reuse key/value. Child list gets updated, so duplicate
+            copy = hamt(self._key, self._value, list(self._children))
+            if (copy._children[child_num] == None):
+                # End of search, key not found, add new node
+                copy._children[child_num] = hamt(key, value)
+            else:
+                # Continue by asking appropriate child to set key/value
+                copy._children[child_num] = copy._children[child_num].     \
+                                    _set(key, value, hashbits >> hamt.BITS)
+            return copy  
+    
+    def set(self, key, value):
+        # Pass key/value and hashbits to recursive helper
+        return self._set(key, value, hash(key))
+    
+    def __str__(self):
+        s = "[({},{})".format(str(self._key),str(self._value))
+        for i in range(hamt.DEG):
+            if (self._children[i] == None):
+                s = s + "X"
+            else:
+                s = s + str(self._children[i])
+        return s + "]"
+        
+a = hamt("A", "a")
+b = a.set("B", "b")
+c = b.set("C", "c")
+d = c.set("D", "d")
+e = d.set("E", "e")
+f = e.set("F", "f")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
