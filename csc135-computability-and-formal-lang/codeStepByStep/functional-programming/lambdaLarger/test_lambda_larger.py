9cd3695012eed21d1db1028ef9b8f20e9454ce84
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-02-19 20:29:04 -0800

    Made asserts into test cases

--- csc135-computability-and-formal-lang/codeStepByStep/functional-programming/lambdaLarger/test_lambda_larger.py
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/lambdaLarger/test_lambda_larger.py
@@ -1,8 +1,20 @@
 import lambda_larger_solution as soln
 
-assert soln.f(4, 11) == 11
-assert soln.f(7, 2) == 7
-assert soln.f(3, 5) == 5
-assert soln.f(42, 42) == 42
-assert soln.f(-2, -4) == -2
-assert soln.f(-1, 0) == 0
+
+def test_lambda_larger_solution1():
+    assert soln.f(4, 11) == 11
+
+def test_lambda_larger_solution2():
+    assert soln.f(7, 2) == 7
+
+def test_lambda_larger_solution3():
+    assert soln.f(3, 5) == 5
+
+def test_lambda_larger_solution4():
+    assert soln.f(42, 42) == 42
+
+def test_lambda_larger_solution5():
+    assert soln.f(-2, -4) == -2
+
+def test_lambda_larger_solution6():
+    assert soln.f(-1, 0) == 0
