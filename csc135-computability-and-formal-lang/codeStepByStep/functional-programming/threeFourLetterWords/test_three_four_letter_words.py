33a1d538edba26a1382471b3fd008462e23ecf1b
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-02-19 23:21:15 -0800

    Three_four_letter_words solution and tests work

---
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/threeFourLetterWords/test_three_four_letter_words.py
@@ -0,0 +1,6 @@
+import three_four_letter_words_solution as soln
+
+
+def test_three_four_letter_words_solution():
+    assert soln.three_four_letter_words(
+        ["four", "score", "and", "seven", "years", "ago"]) == ['four', 'and', 'ago']
--- csc135-computability-and-formal-lang/codeStepByStep/functional-programming/threeFourLetterWords/three_four_letter_words_solution.py
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/threeFourLetterWords/three_four_letter_words_solution.py
@@ -1,2 +1,2 @@
-class ThreeFourLetterWords:
-    pass
+def three_four_letter_words(list_of_strings: list[str]) -> list[str]:
+    return [element for element in list_of_strings if len(element) == 3 or len(element) == 4]
