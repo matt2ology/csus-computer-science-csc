7b62a6a325cd5606c57bd654b4fed56f5381e539
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-02-19 20:28:34 -0800

    Fixed test case names

--- csc135-computability-and-formal-lang/codeStepByStep/collections/lists/test_isMagicSquareSolution.py
+++ csc135-computability-and-formal-lang/codeStepByStep/collections/lists/test_isMagicSquareSolution.py
@@ -1,29 +1,29 @@
 import isMagicSquareSolution as soln
 
 
-def test_case_01():
+def test_is_magic_square_case_01():
     assert soln.is_magic_square([[2, 7, 6], [9, 5, 1], [4, 3, 8]]) == True
 
 
-def test_case_02():
+def test_is_magic_square_case_02():
     assert soln.is_magic_square(
         [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]) == True
 
 
-def test_case_03():
+def test_is_magic_square_case_03():
     assert soln.is_magic_square([[42]]) == True
 
 
-def test_case_04():
+def test_is_magic_square_case_04():
     assert soln.is_magic_square([]) == True
 
 
-def test_case_05():
+def test_is_magic_square_case_05():
     assert soln.is_magic_square(
         [[2, 7, 6], [9, 5, 1, 777], [4, 3, 8]]) == False
 
 
-def test_case_06():
+def test_is_magic_square_case_06():
     assert soln.is_magic_square([[2, 7, 6], [9, 5, 1], [4, 1, 8]]) == False
 
 
