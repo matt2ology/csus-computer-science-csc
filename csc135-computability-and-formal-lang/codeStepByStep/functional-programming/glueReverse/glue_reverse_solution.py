9a628b757ad1a1a634c3529c0fcfd3a1d6e4f4b3
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-02-20 17:56:41 -0800

    updated with optimal solution

--- csc135-computability-and-formal-lang/codeStepByStep/functional-programming/glueReverse/glue_reverse_solution.py
+++ csc135-computability-and-formal-lang/codeStepByStep/functional-programming/glueReverse/glue_reverse_solution.py
@@ -1,2 +1,13 @@
-def glue_reverse(list_of_strings: list[str]) -> str:
+from functools import reduce
+
+
+def glue_reverse_simple_approach(list_of_strings: list[str]) -> str:
     return ''.join(reversed(list_of_strings))
+
+
+def glue_reverse_silly_version(list_of_strings: list[str]) -> str:
+    return str(reduce(lambda x, y: x + y, reversed(list_of_strings)) if list_of_strings else "")
+
+
+def glue_reverse(list_of_strings: list[str]) -> str:
+    return reduce(lambda x, y: x + y, reversed(list_of_strings), "")
