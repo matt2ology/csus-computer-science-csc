a72231fe68133db49fca2cf1eba8e32f9ec1bdcd
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-03-29 21:59:32 -0700

    updated solutions

--- csc134-database-management-systems/group-assignment08-group-select/PoliticalPartySolution.sql
+++ csc134-database-management-systems/group-assignment08-group-select/PoliticalPartySolution.sql
@@ -1,5 +1,5 @@
 drop sequence salary;
-drop table candidate;
+drop table mendoza_candidate;
 drop table party;
 
 
@@ -24,7 +24,7 @@ insert into party
 values(4,'');
 
 
-create table candidate
+create table mendoza_candidate
   (
   lname varchar(40)  not null,
   fname varchar(20)  not null,
@@ -35,46 +35,46 @@ create table candidate
 );
 
 
-insert into candidate
+insert into mendoza_candidate
 values('jennet', 'abraham', 'Berkeley, CA.', salary.nextval, '01 feb 1960',1);
 
-insert into candidate
+insert into mendoza_candidate
 values ( 'Green', 'abraham', 'Oakland, CA.',salary.nextval, '01 feb 1964',1 );
 
-insert into candidate
+insert into mendoza_candidate
 values('gren', 'cheryl','Berkeley, CA.',null,'01 feb 1968',2 );
 
-insert into candidate
+insert into mendoza_candidate
 values('greeenr', 'albert', 'Salt Lake City, UT', salary.nextval, '01 feb 1970',2);
 
-insert into candidate
+insert into mendoza_candidate
 values('gran', 'anne', 'Salt Lake City, UT', salary.nextval, '01 feb 1961',3 );
 
 
-insert into candidate
+insert into mendoza_candidate
 values('mama', 'mia', 'pepper City, UT', salary.nextval, '01 feb 1968',null );
 commit;
 
 -- Question 01
 
-SELECT INITCAP(fname) || ', ' || INITCAP(lname) || ', ' || dob AS "Full Information" FROM candidate;
+SELECT INITCAP(fname) || ', ' || INITCAP(lname) || ', ' || dob AS "Full Information" FROM mendoza_candidate;
 
 -- Question 02
 
-SELECT INITCAP(fname), TRUNC(MONTHS_BETWEEN(SYSDATE,DOB)/12) FROM candidate;
+SELECT INITCAP(fname), TRUNC(MONTHS_BETWEEN(SYSDATE,DOB)/12) FROM mendoza_candidate;
 
 -- Question 03
 
-SELECT UNIQUE INITCAP(lname) AS "Last Name" FROM candidate;
+SELECT UNIQUE INITCAP(lname) AS "Last Name" FROM mendoza_candidate;
 
 -- Question 04
 
-SELECT INITCAP(fname) AS "First Name" FROM candidate WHERE TRUNC(MONTHS_BETWEEN(SYSDATE, DOB)/12) > 40;
+SELECT INITCAP(fname) AS "First Name" FROM mendoza_candidate WHERE TRUNC(MONTHS_BETWEEN(SYSDATE, DOB)/12) > 40;
 
 -- Question 05
 
-SELECT INITCAP(fname) AS "First Name", salary FROM candidate WHERE salary BETWEEN 20000 AND 40000 OR salary = NULL;
+SELECT INITCAP(fname) AS "First Name", salary FROM mendoza_candidate WHERE salary BETWEEN 20000 AND 40000 OR salary IS NULL;
 
 -- Question 06
 
-SELECT INITCAP(fname) AS "First Name" FROM candidate WHERE fname LIKE 'c%' AND TRUNC (MONTHS_BETWEEN(SYSDATE, DOB)/12) > 40;
+SELECT INITCAP(fname) AS "First Name" FROM mendoza_candidate WHERE fname LIKE 'c%' AND TRUNC (MONTHS_BETWEEN(SYSDATE, DOB)/12) > 40;
