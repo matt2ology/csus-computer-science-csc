b3a46f123f1734aecd89e3878a14aad9aad98721
Matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-05-10 12:33:29 -0700

    init commit for parsing built tree

--- csc135-computability-and-formal-lang/module08-computability/test_mod08_hw03_solution_recursive_parse.py
+++ csc135-computability-and-formal-lang/module08-computability/test_mod08_hw03_solution_recursive_parse.py
@@ -1,5 +1,61 @@
 import mod08_hw03_solution_recursive_parse
 
+class test_parse_solution:
+
+    def parseD(self, toks):
+        tok = toks.next()
+        if (tok == 'a'):
+            toks.match(tok)
+        elif tok == '(':
+            toks.match(tok)
+            self.parseS(toks)
+        elif (tok in ('+', '-', ')', 'a') or tok == None):
+            pass
+        else:
+            raise Exception
+
+
+    def parseC(self, toks):
+        tok = toks.next()
+        if tok in ('*', '/'):
+            toks.match(tok)
+            self.parseD(toks)
+            self.parseC(toks)
+            toks.match(')')
+        elif (tok in ('+', '-', ')', 'a') or tok == None):
+            pass
+        else:
+            raise Exception
+
+    def parseB(self, toks):
+        tok = toks.next()
+        if tok in ('a', ')'):
+            toks.match(tok)
+            self.parseD(toks)
+            self.parseC(toks)
+        else:
+            raise Exception
+
+    def parseA(self, toks):
+        tok = toks.next()
+        if tok in ('+', '-'):
+            toks.match(tok)
+            self.parseB(toks)
+            self.parseA(toks)
+            toks.match(')')
+        elif (tok == None) or tok in ('a', ')'):
+            pass
+        else:
+            raise Exception
+
+    def parseS(self, toks):
+        tok = toks.next()
+        if tok in ('a', '('):
+            self.parseB(toks)
+            self.parseA(toks)
+        else:
+            raise Exception
+
 
 def test_parse_case_01():
     assert mod08_hw03_solution_recursive_parse.parse("a")
